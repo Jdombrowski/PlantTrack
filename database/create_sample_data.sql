125cd93615bbdf7b6e2efe1f22a8271472f467ca
JonathanDombrowski <jdombrowski@gladly.com>
2020-06-30 13:21:34 -0700

    add initial schema and sample testing data for the database

---
+++ database/create_sample_data.sql
@@ -0,0 +1,30 @@
+-- -- USERS
+INSERT INTO users
+  (username, email, created_at)
+VALUES
+  ('Jonathan_Dombrowski', 'j@j.com', '2017-02-16 18:22:10.846'),
+  ('Kaylee_Moser', 'k@m.com', '2017-04-02 17:11:21.417'),
+  ('Melanie_Peluso', 'm@p.com', '2017-02-21 11:12:32.574'),
+  ('Lola_Doggam', 'l@d.com', '2016-08-13 01:28:43.085'),
+  ('Petey_Doggam', 'p@d.com', '2016-12-07 01:04:39.298'),
+  ('Bob_Burger', 'b@b.com', '2017-04-30 13:26:14.496'),
+  ('Bear_Stuffins', 'b@s.com', '2016-12-12 06:50:07.996'),
+  ('Morgan_Scahumber', 'm@s.com', '2016-08-20 02:19:45.512'),
+  ('Alec696969', 'A@69.com', '2016-06-24 19:36:30.978');
+
+-- Plants 
+INSERT INTO plants
+  (created_at, light_requirement, location_preference, name, type, user_id)
+VALUES
+  ('2017-02-16 18:22:10.846', 4, 1, "Zuko", "Monstera Deliciousa", 1),
+  ('2017-02-17 18:22:10.846', 4, 1, "Azula", "Monstera Deliciousa", 2),
+  ('2017-02-17 18:22:10.846', 4, 1, "Iroh", "Chinese EverGreen", 1),
+  ('2017-02-17 18:22:10.846', 4, 1, "Toph", "Zanziveria", 2),
+  ('2017-02-17 18:22:10.846', 1, 2, "Sokka", "Cherry Tomato", 1),
+  ('2017-02-17 18:22:10.846', 1, 2, "Momo", "Heirloom Tomato", 2);
+
+INSERT INTO comments
+  (comment_text, photo_id, plant_id, user_id)
+VALUES
+  ( "New leaf starting to unfurl", null, 1, 1),
+  ("2 New leaves starting to unfurl, still dry. Ready for propagation for Melanie", null, 2, 1)  
---
+++ database/init.sql
@@ -0,0 +1,76 @@
+CREATE DATABASE plant_track;
+
+USE plant_track;
+
+CREATE TABLE users
+(
+  id INT
+  AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR
+  (255) UNIQUE NOT NULL,
+  email VARCHAR(255) UNIQUE,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE light_levels
+(
+  id INT AUTO_INCREMENT PRIMARY KEY, 
+  light_requirement VARCHAR(255)
+);
+
+CREATE TABLE location_preferences
+(
+  id INT AUTO_INCREMENT PRIMARY KEY, 
+  location_preference VARCHAR(30)
+);
+
+CREATE TABLE plants
+(
+  id INT AUTO_INCREMENT PRIMARY KEY, 
+  created_at TIMESTAMP DEFAULT NOW(),
+  light_requirement INT,
+  location_preference INT,
+  name VARCHAR(255) UNIQUE NOT NULL, 
+  type VARCHAR(255), 
+  user_id INT NOT NULL, 
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (light_requirement) REFERENCES light_levels(id),
+  FOREIGN KEY (location_preference) REFERENCES location_preferences(id)
+);
+
+
+CREATE TABLE photos(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  image_url VARCHAR(255) NOT NULL,
+  user_id INT NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE comments(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  comment_text VARCHAR(255) NOT NULL,
+  photo_id INT,
+  plant_id INT NOT NULL,
+  user_id INT NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (photo_id) REFERENCES photos(id),
+  FOREIGN KEY (plant_id) REFERENCES plants(id)
+);
+
+-- insert constants
+
+INSERT INTO light_levels
+  (light_requirement)
+VALUES
+  ('Full sun'),
+  ('Partial sun'),
+  ('Full shade'),
+  ('Bright without direct light');
+
+INSERT INTO location_preferences
+  (location_preference)
+VALUES
+  ('Indoor'),
+  ('Outdoor');
