c5cb466c5d728f38fdca34eb4d89ed868271116a
JonathanDombrowski <jdombrowski@gladly.com>
2020-07-01 08:18:17 -0700

    Adding initial api path from tutorial. Adding cors and a fetch in the react app

--- api/app.js
+++ api/app.js
@@ -1,8 +1,9 @@
+var cookieParser = require('cookie-parser');
+var cors = require('cors');
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var path = require('path');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,6 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- planttrack/src/App.js
+++ planttrack/src/App.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [responseText, setResponseText] = useState('TEST');
+
+  useEffect(() => {
+    fetch(`http://localhost:9000/testAPI`)
+      .then((res) => {
+        res.text();
+      })
+      .then((text) => {
+        setResponseText(text);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>{responseText}</p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
